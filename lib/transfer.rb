f0e335ee15d6f549ba173d4a48129a55768510c0
tgray017 <tgray017@gmail.com>
2018-11-05 16:41:44 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,9 +26,9 @@ class Transfer
   
   def reverse_transfer
     if status == "complete"
-    @sender.balance += @amount
-    @receiver.balance -= @amount
-    @status = "reversed"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
   end
   
 end
